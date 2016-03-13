b0b5fd91f293598c70da43e9a3bbddf9651d7d93
pbucci <pbucci@gmail.com>
2016-03-12 18:30:25 -0800

    ready to test auto no-thought-put-into-it chunking

--- pos_chunker/chunk_test.py
+++ pos_chunker/chunk_test.py
@@ -1,9 +1,6 @@
+import ast
+import os
 import nltk
-
-def write(file,text):
-    for line in text:
-        file.write(line)
-
 # This grammar is described in the paper by S. N. Kim,
 # T. Baldwin, and M.-Y. Kan.
 # Evaluating n-gram based evaluation metrics for automatic
@@ -19,10 +16,36 @@ grammar = r"""
         # Above, connected with in/of/etc...
         {<NBAR><IN><NBAR>}
 """
-
 chunker = nltk.RegexpParser(grammar)
-sentence = [('Hi', 'NNP'), (',', ','), ('I', 'PRP'), ("'m", 'VBP'), ('Bill', 'NNP'), ("O'Reilly", 'NNP'), ('.', '.'), ('Thanks', 'NNS'), ('for', 'IN'), ('watching', 'VBG'), ('us', 'PRP'), ('tonight', 'NN'), ('on', 'IN'), ('our', 'PRP$'), ('first', 'JJ'), ('edition', 'NN'), ('of', 'IN'), ('2003', 'CD'), ('.', '.'), ('Happy', 'JJ'), ('New', 'NNP'), ('Year', 'NNP'), ('.', '.')]
-tree = str(chunker.parse(sentence))
-out = open('chunk_tester.txt','w+')
-print(tree)
-write(out,tree)
+
+# write tree to file
+def write(file,text):
+    for line in text:
+        file.write(line)
+    return True
+
+# parse_sentence
+def parse_sentence(sentence,out):
+    tree = str(chunker.parse(sentence))
+    write(out,tree)
+    return True
+
+# parse pape
+def parse_page(path,dir):
+    print('Parsing ' + path)
+    with open(dir + path) as file:
+        f = file.readlines()
+    with open('chunked/' + path, 'w+') as out:
+        for l in f:
+            parse_sentence(ast.literal_eval(l),out)
+    return True
+
+# parse_in_dir
+def parse_in_dir(dir):
+    for file in os.listdir(dir):
+        if file.endswith(".txt"):
+            parse_page(file,dir)
+    return True
+
+# remember trailing slash here
+parse_in_dir('../pos_tagger/pos_tagged_transcripts/')
